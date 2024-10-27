ff7727a9fa6b2840c3c45f99420e862c484b4587
Rik <rikmukherji05@gmail.com>
2024-10-26 23:36:30 -0600

    Copied all UI elements from project to projectv2. Created a method to send notifications to users and to display them

--- Project_v2/app/build.gradle.kts
+++ Project_v2/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -36,11 +37,14 @@ android {
 
 dependencies {
 
+    implementation(libs.circleimageview)
+
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.constraintlayout)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
