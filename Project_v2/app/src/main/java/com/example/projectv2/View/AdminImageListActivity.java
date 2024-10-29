6a58cbab1a980a112720d8a27dbdeca34bc44b84
Rik <rikmukherji05@gmail.com>
2024-10-29 01:23:36 -0600

    - Connected nav bar, nav bar elements to Home Screen. - Implemented pagination and dummy events in Home Screen

---
+++ Project_v2/app/src/main/java/com/example/projectv2/View/AdminImageListActivity.java
@@ -0,0 +1,16 @@
+package com.example.projectv2.View;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.projectv2.R;
+
+public class AdminImageListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_image_list);
+    }
+}
